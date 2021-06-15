17585fa9dc60c0de267bffbaf9c66e004a3ee519
JairMarquez1 <a19310186@ceti.mx>
2021-06-14 21:49:29 -0500

    V1.2

--- main.js
+++ main.js
@@ -13,14 +13,12 @@ function add(){
     var row = tabla.insertRow(states+1);
     var cell = row.insertCell(0);
     cell.className = "td2";
-    cell.innerHTML = `<input id="c0" onClick="" type="text" value="" tabindex="1" maxlength="4" placeholder="q${index}"/>`;
-    console.log("nSimbAdd: "+nSimb);
+    cell.innerHTML = `<input onClick="" type="text" value="" tabindex="1" maxlength="4" placeholder="q${index}"/>`;
     for (var i=1; i <= nSimb ; i++){
         cell = row.insertCell(i);
-        cell.innerHTML = `<input id="c1" onClick="" type="text" value="" tabindex="1" maxlength="11" placeholder="δ"/>`;
+        cell.innerHTML = `<input onClick="" type="text" value="" tabindex="1" maxlength="11" placeholder="δ"/>`;
     }
     states++;
-    console.log(states);
 }
 
 function remove(){
@@ -28,13 +26,11 @@ function remove(){
         var tabla = document.getElementById("Table");
         tabla.deleteRow(states);
         states--;
-        console.log("states: "+states);
     }
 }
 
 function cambiarAlfabeto(e){
     nSimb = e.value;
-    console.log("nSimb: "+nSimb);
     if (nSimb > 10){ nSimb = 10; e.value = 10;}
     else if (nSimb < 2) {nSimb = 2; e.value = 2;}
     var tabla = document.getElementById("Table");
@@ -46,30 +42,19 @@ function cambiarAlfabeto(e){
     newTable+=`</tr>`;
     //First row
     newTable+=`<tr><td class="td2"><input type="text" value="" tabindex="1" maxlength="4" placeholder=">q₀"/></td>${'<td><input type="text" value="" tabindex="1" maxlength="11" placeholder="δ"/></td>'.repeat(nSimb)}`;
-    /*for(var h=0 ; h < nSimb ; h++){
-        newTable+= `<td><input type="text" value="" tabindex="1" maxlength="11" placeholder="δ"/></td>`;
-    }*/
     newTable+=`</tr>`;
     //Table Body------------------------------------------------------------------------------------------------------------//
     for (var i=0; i < states-1 ; i++) {   
         newTable+=`<tr><td class="td2"><input type="text" value="" tabindex="1" maxlength="4" placeholder="q${((i < 10)? subs[i+1] : "ₙ")}"/></td>${'<td><input type="text" value="" tabindex="1" maxlength="11" placeholder="δ"/></td>'.repeat(nSimb)}</tr>`;
-        /*for (var j = 0; j < nSimb ; j++) {
-            newTable+=`<td><input type="text" value="" tabindex="1" maxlength="11" placeholder="δ"/></td>`;
-        }*/
     }
     //Table Footer----------------------------------------------------------------------------------------------------------//
     newTable+=`<tr><td class="button" id="removebutton" class="button" onclick="remove()" style="border-radius: 0"> - </td>${'<td></td>'.repeat(nSimb)}</tr>`;
-    /*for (var k = 0; k < nSimb ; k++) {
-        newTable+= `<td></td>`;
-    }*/
     tabla.innerHTML = newTable;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
 function tableToArray(){
     var tabla = document.getElementById("Table").children[0];
     tablaArray = [];
@@ -86,10 +71,12 @@ function tableToArray(){
     return tablaArray;
 }
 
-
 function afndToAfd(){
     //Se obtienen los valores de la tabla en forma de arreglo
     var afnd = tableToArray();
+    var estadosExcluidos = excluirEstados(afnd);
+    estadosExcluidos.push("");
+    console.log("Excluidos",estadosExcluidos);
     //Se obtienen los estados del AFND en un arreglo
     var estados = [];
     for (var i = 0; i < states; i++){
@@ -111,33 +98,23 @@ function afndToAfd(){
         }
         transiciones.push(transiciones_i);
     }
-    console.log("Transiciones: ", transiciones);
-    
+    console.log("Transiciones: ", transiciones);   
     aviso.innerHTML= "Aceptado";
-
-
-
-
 
     //Se realiza la conversión del autómata-------------------
     //estados:      ["p","q",...]
 
     /*transiciones: [ ["p","p,q",...] , 
                       ["q","",...] , ...
-                    ]
-    */
+                    ]                                   */
     var i = 0;
     while(i < transiciones.length){
 
         transiciones_i = transiciones[i];
-        console.log("estadoActual:",estados[i]);
         transiciones_i.forEach(function(transicion, j) {
-            console.log("trans: ",transicion);
             var nuevoEstado = transicion.split(",").join("");
             if(estados.indexOf(nuevoEstado) == -1 && nuevoEstado != ""){  //Si la transición apunta a un estado no existente, se crea ese estado
-                console.log("nuevoest: ", nuevoEstado);
                 estados.push(nuevoEstado);
-                console.log("estados:",estados);
                 var  nuevoEstadoTrans = [];
                 for(var k = 0; k < nSimb ; k++){
                     var nuevaTransicion = [];
@@ -145,19 +122,13 @@ function afndToAfd(){
                         if(estado != "")
                             nuevaTransicion.push(transiciones[estados.indexOf(estado)][k]);  
                     });
-                    console.log("nueva",nuevaTransicion);
                     nuevaTransicion = uniq(nuevaTransicion.join(",").split(","));
-                    console.log("nuevaTrans:",nuevaTransicion.join(""));
                     nuevoEstadoTrans.push(nuevaTransicion.join(","));
                 }
-                console.log("nuevoEstadoTrans: ",nuevoEstadoTrans);
                 transiciones.push(nuevoEstadoTrans);
-                console.log("Transiciones: ",transiciones);
-
             }
         });
         i++;
-
     }
     //Se eliminan las comas
     transiciones.forEach(function(transicion, x) {
@@ -165,15 +136,13 @@ function afndToAfd(){
             transiciones[x][y] = transicion.split(",").join("");
         });
     });
-    console.log("TransicionesFINAL: ",transiciones," ----------------");
-
     //Se genera la nueva tabla
     var aux;
     var div = document.getElementById("tablaAFD");
     var tablaAFD = `<table><tr><td></td>`;
     //Table header
     for(var u = 0; u < nSimb; u++){
-        tablaAFD += `<td>λ</td>`;
+        tablaAFD += `<td>${u}</td>`;
     }
     tablaAFD += `</tr>`;
     //Table body
@@ -186,8 +155,11 @@ function afndToAfd(){
             indexEstadoValidado =  estados.indexOf(transiciones[v][w]);
             //Se incluyen solo los estados sin transiciones vacías
             if(transiciones[v][w] == "") {aux = -1;break;}
+            if(estadosExcluidos.indexOf(estados[v]) != -1){aux = -1;}
             //Tampoco estados con transiciones que apunten a estados con transiciones vacías
-            transiciones[indexEstadoValidado].forEach(function(transicion,d){if (transicion == "") {aux = -1;}});
+            transiciones[indexEstadoValidado].forEach(function(transicion,d){
+                if (estadosExcluidos.indexOf(transicion) != -1) 
+                    {aux = -1;}});
             filaAFD += `<td>${uniq(transiciones[v][w].split("")).join("")}</td>`;
 
         }
@@ -201,12 +173,28 @@ function afndToAfd(){
     div.innerHTML = tablaAFD;
 }
 
-
 function uniq(a) {
     return a.sort().filter(function(item, pos, ary) {
         return !pos || item != ary[pos - 1];
     });
 }
 
-    
-//window.addEventListener("load",start,false);
+function excluirEstados(tabla){
+    console.log("ex");
+    let excluidos =[];
+    for (var a = 0; a < states; a++){
+        tabla.forEach(function(fila,b){
+            for(var c = 1; c < nSimb+1; c++){
+                auxW=0;
+                transValidada = fila[c].split(",");
+                transValidada.forEach(function(element, index){
+                    if (element=="" || excluidos.indexOf(element) != -1)
+                    auxW++;
+                })
+                if(auxW == transValidada.length)
+                    excluidos.push(fila[0]);  
+            }
+        })
+    }
+    return uniq(excluidos);
+}
